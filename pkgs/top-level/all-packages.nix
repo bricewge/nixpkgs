1b2f993ea962b7062a5489c442ba5fdafc989ccb
Brice Waegeneire <brice.wge@gmail.com>
2019-05-12 16:49:03 +0200

    ddcci-driver: init at 0.3.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15826,6 +15826,8 @@ in
 
     cpupower = callPackage ../os-specific/linux/cpupower { };
 
+    ddcci-driver = callPackage ../os-specific/linux/ddcci { };
+
     deepin-anything = callPackage ../os-specific/linux/deepin-anything { };
 
     dpdk = callPackage ../os-specific/linux/dpdk { };
